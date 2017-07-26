007b1d16960a082a7bf17041b1d51c81f6469fa7
otrack <0track@gmail.com>
2017-07-26 11:36:27 +0200

    some more examples

--- src/main/java/org/example/Hello.java
+++ src/main/java/org/example/Hello.java
@@ -3,24 +3,36 @@ package org.example;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import org.infinispan.creson.Factory;
-import org.infinispan.creson.StaticEntity;
+import org.infinispan.creson.Shared;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
 
 public class Hello implements RequestHandler<Map<String,String>, String>{
 
-    @StaticEntity
-    public static List<String> list;
+    private static Factory factory = Factory.get("52.49.189.207:11222"); // CRESON initialization
+
+    Semaphore semaphore = factory.getInstanceOf(Semaphore.class,"semaphore",false, false, new Object[]{1}); // general declaration
+
+    @Shared ArrayList<String> list = new ArrayList<>(); // @shared with empty constructor only
+
+    @Shared AtomicInteger integer = new AtomicInteger(); // *careful* with final class/methods (see below)
+
+    @Shared Lock lock;// does *not* work as this checks thread id
 
     public String handleRequest(Map<String,String> toadd, Context context) {
-        // Initialize CRESON
-        Factory.get("52.49.189.207:11222");
-        list =  new ArrayList<>();
-
-        for(String value: toadd.values()) {
-            list.add(value);
+        try {
+            for (String value : toadd.values()) {
+                semaphore.acquire();
+                list.add(value);
+                semaphore.release();
+            }
+            System.out.println(integer.incrementAndGet()); // method is final so the call is local.
+        }catch(Exception e) {
+            e.printStackTrace();
         }
         return Integer.toString(list.size());
     }

8a09c35599a4a1beebfa65ff19cd3539acc2d15c
otrack <0track@gmail.com>
2017-07-26 11:53:24 +0200

    some more examples

--- src/main/java/org/example/Hello.java
+++ src/main/java/org/example/Hello.java
@@ -6,32 +6,43 @@ import org.infinispan.creson.Factory;
 import org.infinispan.creson.Shared;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
 
-public class Hello implements RequestHandler<Map<String,String>, String>{
+public class Hello implements RequestHandler<Map<String, String>, String> {
 
     private static Factory factory = Factory.get("52.49.189.207:11222"); // CRESON initialization
 
-    Semaphore semaphore = factory.getInstanceOf(Semaphore.class,"semaphore",false, false, new Object[]{1}); // general declaration
+    Semaphore semaphore = factory.getInstanceOf(Semaphore.class, "semaphore", false, false, new Object[]{1}); // general declaration
 
     @Shared ArrayList<String> list = new ArrayList<>(); // @shared with empty constructor only
+    @Shared Map<Integer, Integer> map = new HashMap<>();
 
-    @Shared AtomicInteger integer = new AtomicInteger(); // *careful* with final class/methods (see below)
+    // *Not working examples*
+    @Shared AtomicInteger integer = new AtomicInteger(); // class/methods are final (see below)
+    @Shared Lock lock;// use of the thread id
 
-    @Shared Lock lock;// does *not* work as this checks thread id
-
-    public String handleRequest(Map<String,String> toadd, Context context) {
+    public String handleRequest(Map<String, String> toadd, Context context) {
         try {
             for (String value : toadd.values()) {
-                semaphore.acquire();
                 list.add(value);
-                semaphore.release();
             }
-            System.out.println(integer.incrementAndGet()); // method is final so the call is local.
-        }catch(Exception e) {
+
+            semaphore.acquire();
+            semaphore.release();
+
+            map.put(1, 0);
+
+            /*
+             integer.incrementAndGet(); // method is final so the call is local.
+             lock.lock();
+             lock.unlock(); // raises an exception
+             */
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return Integer.toString(list.size());

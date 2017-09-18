157607ae72d8a915afc93ad454b73f6cdfa8083d
otrack <0track@gmail.com>
2017-09-18 11:29:50 +0200

    tree map example; usage of 'create = true'

--- src/main/java/org/example/Hello.java
+++ src/main/java/org/example/Hello.java
@@ -8,6 +8,8 @@ import org.infinispan.creson.Shared;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
+import java.util.TreeMap;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
@@ -16,16 +18,20 @@ public class Hello implements RequestHandler<Map<String, String>, String> {
 
     private static Factory factory = Factory.get("54.77.4.130:11222"); // CRESON initialization
 
-    Semaphore semaphore = factory.getInstanceOf(Semaphore.class, "semaphore", false, false, new Object[]{1}); // general declaration
+    Semaphore semaphore = factory.getInstanceOf(Semaphore.class, "sem", false, false, new Object[]{1}); // general declaration
 
-    @Shared ArrayList<String> list = new ArrayList<>(); // @shared with empty constructor only
-    @Shared Map<Integer, Integer> map = new HashMap<>();
+    @Shared ArrayList<String> list = new ArrayList<>(); // @Shared works with empty constructor only
+    @Shared Map<Integer, Integer> map = new HashMap<>(); // hash maps are directly backed by the Infinispan cache
+    @Shared TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+
+    @Shared(create = true) ArrayList<Integer> alist  = new ArrayList<>(); // the persistent list is re-created
 
     // *Not working examples*
     @Shared AtomicInteger integer = new AtomicInteger(); // class/methods are final (see below)
     @Shared Lock lock;// use of the thread id
 
     public String handleRequest(Map<String, String> toadd, Context context) {
+        Random random = new Random(System.currentTimeMillis());
         try {
             for (String value : toadd.values()) {
                 list.add(value);
@@ -34,14 +40,22 @@ public class Hello implements RequestHandler<Map<String, String>, String> {
             semaphore.acquire();
             semaphore.release();
 
+            // hash map
             map.put(1, 0);
             map.put(2, 2);
             if (!map.containsKey(3)) {
                 map.put(3,2);
             }
-
             System.out.println("map size = "+map.size());
             System.out.println("new value = "+map.computeIfPresent(3, (k,v) -> v*v));
+
+            // tree map
+            treeMap.put(random.nextInt(), random.nextInt());
+            System.out.println("tree map size = "+treeMap.size());
+
+            // create = true
+            alist.add(random.nextInt());
+            assert alist.size()==1;
 
             /*
              integer.incrementAndGet(); // method is final so the call is local.

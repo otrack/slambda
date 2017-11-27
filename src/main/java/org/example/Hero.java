ecfc85686f4295a3fd23ee3a2bb7420a89bfd73c
hamza.hassine <hamza.hassine@supcom.tn>
2017-11-27 11:09:29 +0100

    no need for creson

--- src/main/java/org/example/Hero.java
+++ src/main/java/org/example/Hero.java
@@ -1,11 +1,9 @@
 package org.example;
 
 
-import org.infinispan.creson.Shared;
-
 public class Hero {
 	
-	@Shared Room room = new Room();
+	Room room = new Room();
 	
 	public String go() {
 		return room.loot();
--- src/test/java/org/example/TreasureTest.java
+++ src/test/java/org/example/TreasureTest.java
@@ -1,24 +1,20 @@
 package org.example;
 
-import org.infinispan.creson.Factory;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.amazonaws.services.lambda.runtime.ClientContext;
-import com.amazonaws.services.lambda.runtime.CognitoIdentity;
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
+
 
 public class TreasureTest {
 	private Hero hero;
     
     @Before
     public void setUp() throws Exception {
-        Factory.get("localhost:11222"); // Creson initialization
+       // Factory.get("localhost:11222"); // Creson initialization
         hero = new Hero();
     }
     @Test
-    public void should_handle_request() {
+    public void should_fetch_treasure() {
     	
         System.out.println(hero.go());
     }

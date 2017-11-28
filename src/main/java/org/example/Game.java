7078df4ad983913548f622c19481c1b4c9e9c05f
hamza.hassine <hamza.hassine@supcom.tn>
2017-11-28 21:57:13 +0100

    add multithreading players

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -1,18 +1,48 @@
 package org.example;
 
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 public class Game {
 
   public static void main(String[] args) {
+    TreeSet<Hero> scoreBoard = new TreeSet<>();
+    
     Graph gr = new Graph(5);
-
-
     gr.addUndirectedPath(0, 1);
     gr.addUndirectedPath(0, 2);
     gr.addUndirectedPath(0, 3);
+    gr.addUndirectedPath(2, 4);
+    gr.addUndirectedPath(1, 3);
     
-    Hero h = new  Hero("hamza", gr);
-    h.play();
-    System.out.println(h);
+    
+    Hero hamza = new Hero("Hamza", gr);
+    Thread hamzaThread = new Thread(hamza); 
+    
+    Hero arbia = new Hero("Arbia", gr);
+    Thread arbiaThread = new Thread(arbia);
+    
+    Hero asma = new Hero("Asma", gr);
+    Thread asmaThread = new Thread(asma);
+    
+    try {
+      hamzaThread.start();
+      arbiaThread.start();
+      asmaThread.start();
+      
+      hamzaThread.join();
+      asmaThread.join();
+      arbiaThread.join();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    
+    scoreBoard.add(asma);
+    scoreBoard.add(hamza);
+    scoreBoard.add(arbia);
+    
+    for(Hero h : scoreBoard)
+      System.out.println(h);   
   }
 
 }
--- src/main/java/org/example/Hero.java
+++ src/main/java/org/example/Hero.java
@@ -2,54 +2,68 @@ package org.example;
 
 import java.util.Random;
 
-public class Hero {
+public class Hero implements Runnable, Comparable<Hero> {
   Room room = new Room(0);
   int score;
-  Graph graph ; 
+  Graph graph;
   String name;
-  
+
   Hero(Graph graph) {
     this.graph = graph;
     score = 0;
-    name ="default"; 
-  }
-  
-  Hero(String name , Graph graph) {
-    this.graph = graph;
-    score = 0;
-    this.name= name;
+    name = "default";
   }
 
-	private int randomRoom(int lowerBound, int upperBound) {
-	  Random r = new Random();
-    return r.nextInt(upperBound-lowerBound) + lowerBound;
-	}
-  public int play(Room r , boolean[] visited) {
-    if(visited[r.id])
+  Hero(String name, Graph graph) {
+    this.graph = graph;
+    score = 0;
+    this.name = name;
+  }
+
+  private int randomRoom(int lowerBound, int upperBound) {
+    Random r = new Random();
+    return r.nextInt(upperBound - lowerBound) + lowerBound;
+  }
+
+  private int play(Room r, boolean[] visited) {
+    if (visited[r.id])
       return 0;
     visited[r.id] = true;
     int score = r.loot();
-    for(Room neighbour : r.adjList) {
-      if(!visited[neighbour.id]) {
+    for (Room neighbour : r.adjList) {
+      if (!visited[neighbour.id]) {
         score += play(neighbour, visited);
       }
     }
     return score;
   }
-	
-	public void play() {
-	  int  taille = graph.getGraphSize();
-	  boolean[] visited = new boolean[taille];
-	  Room room = graph.getRoom(randomRoom(0, taille-1));
-	  this.score= play(room, visited);
-	}
-  
-	public int go() {
-		return room.loot();
-	}
-	
-	public String toString() {
-	  return ("The player "+ this.name + " has "+ this.score + " treasure(s)" );
-	}
-	
+
+  private void play() {
+    int taille = graph.getGraphSize();
+    boolean[] visited = new boolean[taille];
+    Room room = graph.getRoom(randomRoom(0, taille - 1));
+    this.score = play(room, visited);
+  }
+
+  public int go() {
+    return room.loot();
+  }
+
+  public String toString() {
+    return ("The player " + this.name + " has " + this.score + " treasure(s)");
+  }
+
+  @Override
+  public void run() {
+    play();
+  }
+
+  @Override
+  public int compareTo(Hero other) {
+    if(this.score - other.score != 0)
+      return -(this.score - other.score) ;
+    else
+      return this.name.compareTo(other.name);
+  }
+
 }
